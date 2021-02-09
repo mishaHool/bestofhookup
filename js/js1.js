3f2c54baebcf8fd47a8aabfd97d207ff130a51f6
Miha <vinemaster228@gmail.com>
2021-02-09 22:30:07 +0200

    404-patch

---
+++ js/js1.js
@@ -0,0 +1,10 @@
+let cook = document.getElementById('cookie-btt');
+let cookBlock = document.getElementById('cookie')
+if (!document.cookie.includes(click = true)) {
+    cook.addEventListener('click', function() {
+        document.cookie = 'click=true';
+        cookBlock.style.display = 'none';
+    })
+} else {
+    cookBlock.style.display = 'none'
+}
